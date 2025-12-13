2ea0a63f4c5d48928801e005ba89366419d108e4
Spencer Raymond <spencer@spencerraymon.de>
2025-12-13 17:55:13 +1100

    implement tidal! linkgrabbr done mostly

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod apple_music;
 mod spotify;
+mod tidal;
 
 use crate::apple_music::AppleMusicClient;
 use crate::spotify::SpotifyClient;
@@ -7,6 +8,7 @@ use futures::try_join;
 use serde::{Deserialize, Serialize};
 use worker::{Context, Env, Request, Response, Router};
 
+use crate::tidal::TidalClient;
 use worker_macros::event;
 
 #[event(fetch)]
@@ -30,26 +32,43 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> worker::Result<Response
             if let Some(upc) = ctx.param("upc") {
                 let upc = upc.to_string();
                 let apple_music_client = AppleMusicClient::new();
-                let spotify_client = match SpotifyClient::new(
-                    &ctx.secret("SPOTIFY_CLIENT_ID")
-                        .expect("No spotify client ID.")
-                        .to_string(),
-                    &ctx.secret("SPOTIFY_CLIENT_SECRET")
-                        .expect("No spotify client secret.")
-                        .to_string(),
-                )
-                .await
-                {
-                    Ok(client) => client,
-                    Err(e) => return Response::error(e.to_string(), 500),
-                };
 
-                let Ok((spotify_release, apple_music_release)) = try_join!(
-                    spotify_client.get_release_by_upc(&upc),
-                    apple_music_client.get_release_by_upc(&upc)
+                let spotify_client_id = ctx
+                    .secret("SPOTIFY_CLIENT_ID")
+                    .expect("No spotify client ID.")
+                    .to_string();
+
+                let spotify_client_secret = ctx
+                    .secret("SPOTIFY_CLIENT_SECRET")
+                    .expect("No spotify client secret.")
+                    .to_string();
+
+                let tidal_client_id = ctx
+                    .secret("TIDAL_CLIENT_ID")
+                    .expect("No tidal client ID")
+                    .to_string();
+
+                let tidal_client_secret = ctx
+                    .secret("TIDAL_CLIENT_SECRET")
+                    .expect("No tidal client secret")
+                    .to_string();
+
+                let Ok((spotify_client, tidal_client)) = try_join!(
+                    SpotifyClient::new(&spotify_client_id, &spotify_client_secret),
+                    TidalClient::new(&tidal_client_id, &tidal_client_secret)
                 ) else {
                     return Response::error("Internal Server Error".to_string(), 500);
                 };
+
+                let Ok((spotify_release, apple_music_release, tidal_release)) = try_join!(
+                    spotify_client.get_release_by_upc(&upc),
+                    apple_music_client.get_release_by_upc(&upc),
+                    tidal_client.get_release_by_upc(&upc)
+                ) else {
+                    return Response::error("Internal Server Error".to_string(), 500);
+                };
+
+                let tidal_link = tidal_release.externalLinks.get(0).unwrap().href.clone();
 
                 let album_art = apple_music_release.get_album_art_url(750);
 
@@ -61,7 +80,7 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> worker::Result<Response
                     image_url: Some(album_art),
                     artist_name: Some(apple_music_release.artistName),
                     spotify: Some(spotify_release.external_urls.spotify),
-                    tidal: None,
+                    tidal: Some(tidal_link),
                 };
 
                 return Response::from_json(&response);
---
+++ src/tidal.rs
@@ -0,0 +1,119 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+struct RawSearchResponse {
+    pub data: Vec<RawReleaseResponse>,
+    links: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct RawReleaseResponse {
+    id: String,
+    r#type: String,
+    pub attributes: TidalRelease,
+    relationships: Relationships,
+}
+#[derive(Serialize, Deserialize, Default)]
+#[allow(non_snake_case)]
+pub struct TidalRelease {
+    title: String,
+    barcodeId: String,
+    numberOfVolumes: u32,
+    numberOfItems: u32,
+    duration: String,
+    explicit: bool,
+    releaseDate: String,
+    copyright: HashMap<String, String>,
+    popularity: f64,
+    accessType: String,
+    availability: Vec<String>,
+    mediaTags: Vec<String>,
+    pub externalLinks: Vec<ExternalLinks>,
+    r#type: String,
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct ExternalLinks {
+    pub href: String,
+    meta: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Links {
+    links: HashMap<String, String>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[allow(non_snake_case)]
+struct Relationships {
+    artists: Links,
+    coverArt: Links,
+    genres: Links,
+    items: Links,
+    owners: Links,
+    providers: Links,
+    similarAlbums: Links,
+    suggestedCoverArts: Links,
+}
+
+pub struct TidalClient {
+    client: reqwest::Client,
+    access_token: String,
+}
+
+impl TidalClient {
+    pub async fn new(client_id: &str, secret: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        #[derive(Serialize, Deserialize)]
+        struct TokenResponse {
+            pub access_token: String,
+            pub token_type: String,
+            pub expires_in: u32,
+        }
+
+        let client = reqwest::Client::new();
+
+        let resp = client
+            .post("https://auth.tidal.com/v1/oauth2/token")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(format!(
+                "grant_type=client_credentials&client_id={client_id}&client_secret\
+            ={secret}"
+            ))
+            .send()
+            .await?
+            .json::<TokenResponse>()
+            .await?;
+
+        let access_token = format!("{} {}", resp.token_type, resp.access_token);
+
+        Ok(Self {
+            client,
+            access_token,
+        })
+    }
+
+    pub async fn get_release_by_upc(
+        &self,
+        upc: &str,
+    ) -> Result<TidalRelease, Box<dyn std::error::Error>> {
+        let resp = self
+            .client
+            .get(format!(
+                "https://openapi.tidal.com/v2/albums?countryCode=US&filter%5BbarcodeId%5D={upc}"
+            ))
+            .header("Authorization", &self.access_token)
+            .header("accept", "application/vnd.api+json")
+            .send()
+            .await?
+            .json::<RawSearchResponse>()
+            .await
+            .unwrap();
+
+        if let Some(album) = resp.data.into_iter().nth(0).take() {
+            Ok(album.attributes)
+        } else {
+            Ok(TidalRelease::default())
+        }
+    }
+}
