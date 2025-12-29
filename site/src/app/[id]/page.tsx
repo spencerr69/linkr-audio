6785aa60f7d879c7070af13ce9665e0420b6982c
Spencer Raymond <spencer@spencerraymon.de>
2025-12-29 20:35:27 +1100

    feat(site): redirect when going to {id}.url/

---
+++ site/src/app/[id]/page.tsx
@@ -0,0 +1,17 @@
+"use server";
+
+import { getLatestRelease } from "@/app/actions/releases";
+import { redirect } from "next/navigation";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const latest_release = await getLatestRelease(id);
+
+  redirect(`/${latest_release.slug}`);
+
+  return <></>;
+}
