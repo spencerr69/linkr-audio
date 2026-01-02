64371d488a50bf473f7670bdcce8b498ad8cdf67
Spencer Raymond <spencer@spencerraymon.de>
2026-01-02 20:25:43 +1100

    fix(session): return null on failed session verification

--- site/src/lib/session.ts
+++ site/src/lib/session.ts
@@ -25,7 +25,8 @@ export const decrypt = async (session: string | undefined = "") => {
     return payload;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (_) {
-    return {};
+    console.log("Couldn't verify session.");
+    return null;
   }
 };
 
