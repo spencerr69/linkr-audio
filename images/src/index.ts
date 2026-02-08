ac8bf098322e1e53ffc03c1ccb6ec1db3f861749
Spencer Raymond <spencer@spencerraymon.de>
2026-02-08 23:09:11 +1100

    chore(images): handle CORS

--- images/src/index.ts
+++ images/src/index.ts
@@ -13,7 +13,43 @@
 
 export default {
    async fetch(request, env, ctx): Promise<Response> {
-      let url = new URL(request.url);
+      const corsHeaders = {
+         "Access-Control-Allow-Origin": "*",
+         "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+         "Access-Control-Max-Age": "86400",
+      };
+      
+      async function handleOptions(request: Request) {
+         if (
+            request.headers.get("Origin") !== null &&
+            request.headers.get("Access-Control-Request-Method") !== null &&
+            request.headers.get("Access-Control-Request-Headers") !== null
+         ) {
+            // Handle CORS preflight requests.
+            return new Response(null, {
+               headers: {
+                  ...corsHeaders,
+                  "Access-Control-Allow-Headers": request.headers.get(
+                     "Access-Control-Request-Headers",
+                  ) || "",
+               },
+            });
+         } else {
+            // Handle standard OPTIONS request.
+            return new Response(null, {
+               headers: {
+                  Allow: "GET, HEAD, POST, OPTIONS",
+               },
+            });
+         }
+      }
+      
+      const url = new URL(request.url);
+      if (request.method === "OPTIONS") {
+         // Handle CORS preflight requests
+         return handleOptions(request);
+      }
+      
       
       if (url.pathname.endsWith("upload")) {
          const image = await request.formData();
