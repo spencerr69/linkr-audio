1de69a45a1a9ca0701b29254fc8b286d1082721b
Spencer Raymond <spencer@spencerraymon.de>
2026-06-03 18:02:41 +1000

    chore(site): update posthog implementation to support a reverse proxy

--- site/instrumentation-client.ts
+++ site/instrumentation-client.ts
@@ -2,7 +2,8 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "",
-  defaults: "2025-05-24",
+  ui_host: "https://eu.posthog.com",
+  defaults: "2026-05-30",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
 });
